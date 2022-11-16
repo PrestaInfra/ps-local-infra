38391d7e37f7ecc1d427b1ab1fed36437e9df8da
PululuK <pululuandre@hotmail.com>
2022-11-16 04:35:28 +0100

    Run shell script only for advanced containers

--- entrypoints/ps-container-template/setup.sh
+++ entrypoints/ps-container-template/setup.sh
@@ -4,11 +4,14 @@ CONTAINER_ALREADY_STARTED=/home/entrypoints/var/CONTAINER_ALREADY_STARTED
 
 if [ ! -e $CONTAINER_ALREADY_STARTED ]
 then
-  if [ ! -z "PS_ENTRY_POINT_SCRIPT_URL" ]; then
-    echo "[⏳] - Running shell script ${PS_ENTRY_POINT_SCRIPT_URL} ..."
-    sh -c "$(wget $PS_ENTRY_POINT_SCRIPT_URL -O -)"
+  if [ "$IS_ADVANCED_CONTAINER" = "1" ]; then
+    if [ ! -z "PS_ENTRY_POINT_SCRIPT_URL" ]; then
+        echo "[⏳] - Running shell script ${PS_ENTRY_POINT_SCRIPT_URL} ..."
+        sh -c "$(wget $PS_ENTRY_POINT_SCRIPT_URL -O -)"
+      else
+        echo "[⚠️] - Missing shell script : ${PS_ENTRY_POINT_SCRIPT_URL}"
+    fi
   else
-    echo "[⚠️] - Missing shell script : ${PS_ENTRY_POINT_SCRIPT_URL}"
+    echo "[⚠️] - Is a empty container. You need to install you project manually"
   fi
 fi
-
